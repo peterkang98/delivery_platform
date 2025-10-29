1a47846772216370a7bc6e38c81e55ceaf233e72
peterkang98 <peterkang98@gmail.com>
2025-10-29 09:57:50 +0900

    initial commit

---
+++ src/test/java/xyz/sparta_project/manjok/ManjokApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sparta_project.manjok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManjokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
