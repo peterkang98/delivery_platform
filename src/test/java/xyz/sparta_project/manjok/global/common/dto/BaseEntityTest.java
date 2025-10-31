8fe7a133cb4049c6cf5b1935d52f57dfcef07b57
jun <pickjog@gmail.com>
2025-10-31 10:01:22 +0900

    refactor: Convert BaseEntity to abstract class and add TestEntity for Testing

--- src/test/java/xyz/sparta_project/manjok/global/common/dto/BaseEntityTest.java
+++ src/test/java/xyz/sparta_project/manjok/global/common/dto/BaseEntityTest.java
@@ -10,12 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("BaseEntity")
 class BaseEntityTest {
+    
+    /**
+     * 테스트용 구체 클래스
+     * */
+    static class TestEntity extends BaseEntity {
+        // BaseENtity의 기능만 테스트하므로 추가 필드 불필요
+    }
 
     @Test
     @DisplayName("ID를 자동 생성한다.")
     void onCreate_generates_id() {
         //Given & When
-        BaseEntity entity = new BaseEntity();
+        TestEntity entity = new TestEntity();
 
         //Then
         assertThat(entity.getId()).isNotNull();
@@ -25,7 +32,7 @@ class BaseEntityTest {
     @DisplayName("생성된 ID는 36자다.")
     void generated_id_is_36_chars() {
         //Given & When
-        BaseEntity entity = new BaseEntity();
+        TestEntity entity = new TestEntity();
 
         //Then
         assertThat(entity.getId()).hasSize(36);
@@ -35,7 +42,7 @@ class BaseEntityTest {
     @DisplayName("생성된 ID는 유효한 UUID이다.")
     void generated_id_is_valid_uuid() {
         //Given & When
-        BaseEntity entity = new BaseEntity();
+        TestEntity entity = new TestEntity();
 
         //Then
         assertThat(UuidUtils.isValid(entity.getId())).isTrue();
@@ -45,7 +52,7 @@ class BaseEntityTest {
     @DisplayName("createdAt을 자동 설정한다.")
     void onCreate_sets_created_at() {
         //Given & When
-        BaseEntity entity = new BaseEntity();
+        TestEntity entity = new TestEntity();
 
         //Then
         assertThat(entity.getCreatedAt()).isNotNull();
@@ -56,7 +63,7 @@ class BaseEntityTest {
     void created_at_is_current_server_time() {
         //Given
         LocalDateTime before = LocalDateTime.now();
-        BaseEntity entity = new BaseEntity();
+        TestEntity entity = new TestEntity();
 
         //Then
         LocalDateTime after = LocalDateTime.now();
@@ -70,8 +77,8 @@ class BaseEntityTest {
     @DisplayName("매번 다른 ID를 생성한다.")
     void generates_unique_ids() {
         //Given & When
-        BaseEntity entity1 = new BaseEntity();
-        BaseEntity entity2 = new BaseEntity();
+        TestEntity entity1 = new TestEntity();
+        TestEntity entity2 = new TestEntity();
 
         //Then
         assertThat(entity1.getId()).isNotEqualTo(entity2.getId());
@@ -81,7 +88,7 @@ class BaseEntityTest {
     @DisplayName("이미 ID와 createdAt가 있으면 새로 생성하지 않는다.")
     void does_not_override_existing_id() {
         //Given & When
-        BaseEntity entity = new BaseEntity();
+        TestEntity entity = new TestEntity();
         String existingId = entity.getId();
         LocalDateTime existingCreatedAt = entity.getCreatedAt();
 
@@ -94,8 +101,8 @@ class BaseEntityTest {
     @DisplayName("같은 ID를 가진 엔티티는 같다.")
     void entities_with_same_id_are_equal() throws Exception {
         //Given & When
-        BaseEntity entity1 = new BaseEntity();
-        BaseEntity entity2 = new BaseEntity();
+        TestEntity entity1 = new TestEntity();
+        TestEntity entity2 = new TestEntity();
 
         var field = BaseEntity.class.getDeclaredField("id");
         field.setAccessible(true);
@@ -111,8 +118,8 @@ class BaseEntityTest {
     @DisplayName("다른 ID를 가진 엔티티는 다르다.")
     void entities_with_different_ids_are_not_equal() {
         //Given & When
-        BaseEntity entity1 = new BaseEntity();
-        BaseEntity entity2 = new BaseEntity();
+        TestEntity entity1 = new TestEntity();
+        TestEntity entity2 = new TestEntity();
 
         //When
         assertThat(entity1).isNotEqualTo(entity2);
